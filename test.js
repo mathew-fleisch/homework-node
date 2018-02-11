ba74a27cf9749804bff365e6aab40218d1a3ded1
Mathew Fleisch <mathew.fleisch@gmail.com>
2018-02-11 11:30:18 -0800

    Fix ‘verifyCount’ test for packages with sub-packages

--- test.js
+++ test.js
@@ -5,11 +5,13 @@ const series = require('run-series')
 const fs = require('fs')
 const folderSize = require('get-folder-size')
 const download = require('./')
+const async = require('async')
 
 test('download', function (t) {
   t.plan(3)
 
   const COUNT = parseInt(process.env.COUNT, 10) || 10
+  console.log('Expecting Count: '+COUNT)
 
   series([
     (callback) => download(COUNT, callback),
@@ -20,12 +22,37 @@ test('download', function (t) {
 
   function verifyCount (callback) {
     fs.readdir('./packages', function (err, files) {
-      if (err) return callback(err)
-      // Filter .gitignore and other hidden files
-      files = files.filter((file) => !/^\./.test(file))
-      t.equal(files.length, COUNT, `has ${COUNT} files`)
-      callback()
-    })
+      if (err) return callback(err);
+
+      const sub_packages = [];
+      files = files.filter((file) => {
+        if (file.match(/^\@/)) {
+          sub_packages.push(file);
+          return false;
+        }
+        return !/^\./.test(file);
+      });
+
+      // check the existance of sub-packages
+      async.each(sub_packages, (name, cb) => {
+        fs.readdir(`./packages/${name}`, (err, sub_files) => {
+          if (err) return cb(err);
+          sub_files = sub_files.filter((sub_file) => {
+              if (!/^\./.test(sub_files)) {
+                files.push(`${name}/${sub_file}`);
+                return true;
+              }
+              return false;
+          });
+
+          cb();
+        });
+      }, (err) => {
+        if(err) return callback(err);
+        t.equal(files.length, COUNT, `has ${COUNT} files`);
+        callback();
+      });
+    });
   }
 
   function verifySize (callback) {
